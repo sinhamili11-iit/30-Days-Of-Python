021a200219ddd12f36acf2646d9cb178f38fc55d
sinhamili11-iit <sinhamili11@gmail.com>
2026-04-03 12:00:00 +0530

    Day 01: Initial setup and basics

---
+++ Day 01 Challenges/day1_challenge2.py
@@ -0,0 +1,3 @@
+numbers = [1, 22, 33, 44, 55, 66, 77, 88, 99, 101]
+even_numbers= [num for num in numbers if num % 2 ==0]
+print(f"The even numbers are = {even_numbers}")
---
+++ Day 01 Challenges/day1_challenge3.py
@@ -0,0 +1,2 @@
+word= input("Enter any word = ")
+print("The Reverse word is ", word[::-1])
