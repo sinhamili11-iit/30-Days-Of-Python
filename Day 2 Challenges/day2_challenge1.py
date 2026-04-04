4b7a0cf017669a13c575e1494a6b5eca53eba599
sinhamili11-iit <sinhamili11@gmail.com>
2026-04-04 12:00:00 +0530

    Maintenance: Cleaned up old Day 2 folder

--- Day 2 Challenges/day2_challenge1.py
+++
@@ -1,7 +0,0 @@
-shopping_list =[]
-shopping_list.append("Milk")
-shopping_list.append("Egg")
-shopping_list.append("Bread")
-shopping_list.insert(0, "Butter")
-shopping_list.remove("Egg")
-print(shopping_list, len(shopping_list))
--- Day 2 Challenges/day2_challenge2.py
+++
@@ -1,8 +0,0 @@
-char_count ={}
-word = input("Enter any word= ")
-for char in word:
-    if char in char_count:
-        char_count[char] = char_count[char] + 1
-    else:
-        char_count[char] = 1
-print(char_count)
