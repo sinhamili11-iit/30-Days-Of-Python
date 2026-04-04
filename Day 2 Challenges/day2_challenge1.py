780dddfcecb6077f59639eba84958a12e3bf7fa0
sinhamili11-iit <sinhamili11@gmail.com>
2026-04-04 23:52:23 +0530

    Day 2: Lists and Dictionaries completed

---
+++ Day 2 Challenges/day2_challenge1.py
@@ -0,0 +1,7 @@
+shopping_list =[]
+shopping_list.append("Milk")
+shopping_list.append("Egg")
+shopping_list.append("Bread")
+shopping_list.insert(0, "Butter")
+shopping_list.remove("Egg")
+print(shopping_list, len(shopping_list))
